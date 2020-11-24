8eea4bd20b3a7f33f3ff2990cde1061c389471fb
Julino Segunda Dídimo <ddimojulino@gmail.com>
2020-11-24 02:46:33 -0300

    Good to see you again, darling

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cryptocoin
+
+![dashboard](readme.png)
+
+Reading this code you will learn the basic of fetching, with `Fetch API`, taking some information from [coinmarketcap](https://coinmarketcap.com/yield-farming/) site, also, you will learn, as i did, the basic of promises, actualy you will see it in are `Javascript` file represented by `then()` method.
+
+#### Take a look, and enjoy.
--- src/reqs.js
+++ src/reqs.js
@@ -10,23 +10,21 @@ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='
         return response.json()
     })
     .then((api) => {
-        const text = ''
-        for(let i = 0; i >= 10; i++){
+        var text = ''
+        for(let i = 0; i < 10; i++){
+            
+            console.log(api)
 
             text = text + `
-            <div className="media">
-                <img src = 'coin.jpg' class= 'align-self-center mr-3' alt = 'coin' width='100' height='60'>
-                <div className="media-body">
-                    <h5 className="mt-2">${api.data[i].name}</h5>
+            <div class="media">
+                <img src = '' class= 'align-self-center mr-3' alt = 'coin' width='100' height='60'>
+                <div class="media-body">
+                    <h5 class="mt-2">${api.data[i].name}</h5>
                     <p>${api.data[i].symbol}</p>
+                    <p>${api.data[i].first_historical_data}</p>
                 </div>
             </div>
             `
-            document.getElementById('coin').innerHTML = text
+            document.getElementById('coins').innerHTML = text
         }
-        console.log(api)
     })
-    .catch((error) => {
-        console.error(error.message)
-    })
-
